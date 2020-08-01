39d6580e18f73b92a83c0c597496bb58891a70cc
oksana sam <oksana.o.samokhvalova@gmail.com>
2020-07-31 22:40:22 -0400

    new

---
+++ build/precache-manifest.a60b0b6205d03e15b4e75b30793d3153.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96d768667cf064e3a0a497ec7ca8cc33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc88b3efc02c16a6776c",
+    "url": "/static/css/2.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "638a19d86a8c36f1fc5a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fc88b3efc02c16a6776c",
+    "url": "/static/js/2.e435b808.chunk.js"
+  },
+  {
+    "revision": "c1823a4f1fe4309f3ff3cc56b4eb0a86",
+    "url": "/static/js/2.e435b808.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "638a19d86a8c36f1fc5a",
+    "url": "/static/js/main.c8263417.chunk.js"
+  },
+  {
+    "revision": "5aae3d4ac965d7a41bc6",
+    "url": "/static/js/runtime-main.d5a61aa8.js"
+  }
+]);
