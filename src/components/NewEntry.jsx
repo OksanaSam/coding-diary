c217347e25ed5eecc9c5b71ff039be61ef21d9cf
Oksana Sam <oksana.o.samokhvalova@gmail.com>
2020-09-06 23:10:10 -0400

    added autoformatted code

--- src/components/NewEntry.jsx
+++ src/components/NewEntry.jsx
@@ -4,167 +4,146 @@ import firebase from 'firebase/app';
 import Swal from 'sweetalert2';
 import DatePicker from 'react-datepicker';
 import { format, compareAsc } from 'date-fns';
-import SelectedTools from './SelectedTools'
-
+import SelectedTools from './SelectedTools';
 
 const NewEntry = (props) => {
-    const [isChecked, setChecked] = useState(false);
-    const [textArea, setTextArea] = useState('');
+  const [isChecked, setChecked] = useState(false);
+  const [textArea, setTextArea] = useState('');
 
-   
-    
-    const [selectedOptions, setSelectedOptions] = useState(
-        JSON.parse(localStorage.getItem('selectedTools')) || []
-    );
+  const [selectedOptions, setSelectedOptions] = useState(
+    JSON.parse(localStorage.getItem('selectedTools')) || []
+  );
 
-    const [inputValue, setInputValue] = useState('');
-    const [items, setItems] = useState(
-        JSON.parse(localStorage.getItem('items')) || []
-    );
+  const [inputValue, setInputValue] = useState('');
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('items')) || []);
 
-    useEffect(() => {
-        setChecked(props.isGlobalChecked); 
-        localStorage.setItem('selectedTools', JSON.stringify(selectedOptions));
-        localStorage.setItem('items', JSON.stringify(items));
+  useEffect(() => {
+    setChecked(props.isGlobalChecked);
+    localStorage.setItem('selectedTools', JSON.stringify(selectedOptions));
+    localStorage.setItem('items', JSON.stringify(items));
+  }, [props.isGlobalChecked, selectedOptions, items]);
 
-    }, [props.isGlobalChecked, selectedOptions, items]);
+  const handleChecked = () => {
+    setChecked(!isChecked);
+  };
 
+  const handleTextAreaChange = (e) => {
+    setTextArea(e.target.value);
+    console.log(textArea);
+  };
 
-    const handleChecked = () => {
-        setChecked(!isChecked);
-    };
-
-    const handleTextAreaChange = (e) => {
-        setTextArea(e.target.value);
-        console.log(textArea)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!inputValue) {
+      Swal.fire({
+        title: 'Hm...',
+        text: 'Please enter your text',
+        confirmButtonText: 'Ok',
+      });
+    } else {
+      // const newItems = [...items];
+      // newItems.push({ text: inputValue, done: false });
+      setItems([...items, { text: inputValue, done: false }]);
+      localStorage.setItem('items', JSON.stringify(items));
+      setInputValue('');
     }
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (!inputValue) {
-            Swal.fire({
-                title: 'Hm...',
-                text: 'Please enter your text',
-                confirmButtonText: 'Ok',
-            });
-        } else {
-            // const newItems = [...items];
-            // newItems.push({ text: inputValue, done: false });
-            setItems([...items, { text: inputValue, done: false }]);
-            localStorage.setItem('items', JSON.stringify(items))
-            setInputValue('');
+  const handleCardSubmit = async () => {
+    if (!props.user) {
+      Swal.fire({
+        title: 'Oops...',
+        text: 'Please sign in',
+        confirmButtonText: 'Ok',
+      });
+    } else if (items.length < 1) {
+      Swal.fire({
+        title: 'Oops...',
+        text: 'Please add at least one entry!',
+        confirmButtonText: 'Ok',
+      });
+    } else {
+      const dbRef = await firebase.database().ref(`users/${props.user}`);
+      console.log(dbRef);
+      const obj = { tags: selectedOptions, entries: items };
+      if (dbRef === null) {
+        firebase
+          .database()
+          .ref('users/' + props.user)
+          .set(obj);
+      } else {
+        dbRef.push(obj);
+      }
+      console.log('added');
+      setItems([]);
+      setSelectedOptions([]);
+
+      dbRef.on('value', (snapshot) => {
+        const data = snapshot.val();
+
+        console.log('response from database', data);
+
+        const entryList = [];
+        for (let key in data) {
+          entryList.push({
+            log: data[key],
+            uniqueId: key,
+          });
         }
+        props.handleCardsAdd(entryList);
+        // setItems(entryList)
+      });
     }
+  };
 
-    
-    const handleCardSubmit = async () => {
-        if (!props.user) {
-            Swal.fire({
-              title: 'Oops...',
-              text: 'Please sign in',
-              confirmButtonText: 'Ok',
-            })
-        } else if (items.length < 1) {
-            Swal.fire({
-                title: 'Oops...',
-                text: 'Please add at least one entry!',
-                confirmButtonText: 'Ok',
-            });
-        } else {
-            const dbRef = await firebase.database().ref(`users/${props.user}`)
-            console.log(dbRef)
-            const obj = { tags: selectedOptions, entries: items }
-            if (dbRef === null) {
-                firebase.database().ref('users/' + props.user).set(obj)
-            } else {
-                dbRef.push(obj);
-            }
-            console.log('added');
-            setItems([]);
-            setSelectedOptions([]);
- 
-            dbRef.on('value', (snapshot) => {
-                const data = snapshot.val();
-          
-                console.log('response from database', data);
-                
-                const entryList = [];
-                for (let key in data) {
-                  entryList.push({
-                    log: data[key],
-                    uniqueId: key
-                  });
-                }
-                props.handleCardsAdd(entryList)
-                // setItems(entryList)
-              })
-        }
-    }
+  const handleClick = (index) => {
+    const newItems = [...items];
+    newItems[index].done = !newItems[index].done;
+    setItems(newItems);
+  };
 
-    const handleClick = (index) => {
-        const newItems = [...items];
-        newItems[index].done = !newItems[index].done;
-        setItems(newItems);
-    };
+  const handleDelete = (index) => {
+    const newItems = [...items];
+    newItems.splice(index, 1);
+    setItems(newItems);
+  };
 
-    const handleDelete = (index) => {
-        const newItems = [...items];
-        newItems.splice(index, 1);
-        setItems(newItems);
-    };
+  return (
+    <>
+      <SelectedTools selectedOptions={selectedOptions} setSelectedOptions={setSelectedOptions} />
+      <p>{props.item}</p>
+      {/* <p>{props.currentDate ? `${props.currentDate.getMonth()} ${props.currentDate.getDate()} ${props.currentDate.getFullYear()}` : null}</p> */}
+      <p>{props.currentDate ? format(props.currentDate, 'do MMMM yyyy') : null}</p>
+      <input type="checkbox" onChange={() => handleChecked(props.item)} checked={isChecked} />
+      <button onClick={() => handleDelete(props.item)}>delete</button>
+      <DatePicker
+        styles={{ backgroundColor: 'blue' }}
+        selected={props.currentDate}
+        onChange={props.handleDateChange}
+        onSelect={props.handleDateSelect}
+      />
 
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+        </label>
+        <button>Add</button>
+      </form>
 
-    return (
-        <>
-            <SelectedTools
-                selectedOptions={selectedOptions}
-                setSelectedOptions={setSelectedOptions}
-            />
-            <p>{props.item}</p>
-            {/* <p>{props.currentDate ? `${props.currentDate.getMonth()} ${props.currentDate.getDate()} ${props.currentDate.getFullYear()}` : null}</p> */}
-            <p>{props.currentDate ? format(props.currentDate, 'do MMMM yyyy') : null}</p>
-            <input
-                type='checkbox'
-                onChange={() => handleChecked(props.item)}
-                checked={isChecked}
-            />
-             <button onClick={() => handleDelete(props.item)}>delete</button>
-             <DatePicker
-                styles={ {backgroundColor: 'blue'} }
-                selected={props.currentDate}
-                onChange={props.handleDateChange}
-                onSelect={props.handleDateSelect}
-            />
-
-        <form onSubmit={handleSubmit}>
-          <label>
-            <input
-              type='text'
-              value={inputValue}
-              onChange={(e) => setInputValue(e.target.value)}
-            />
-          </label>
-          <button>Add</button>
-        </form>
-
-        <ul>
-          {items.map((item, id) => (
-            <li key={id}>
-              <label>
-                <input
-                  type='checkbox'
-                  checked={item.done}
-                  onClick={() => handleClick(id)}
-                />
-                <span className={item.done ? 'done' : null}>{item.text}</span>
-                <button onClick={() => handleDelete(id)}>X</button>
-              </label>
-              </li>
-          ))}
-        </ul>
-        <button onClick={handleCardSubmit}>Save Entry</button>
-        </>   
-    );
+      <ul>
+        {items.map((item, id) => (
+          <li key={id}>
+            <label>
+              <input type="checkbox" checked={item.done} onClick={() => handleClick(id)} />
+              <span className={item.done ? 'done' : null}>{item.text}</span>
+              <button onClick={() => handleDelete(id)}>X</button>
+            </label>
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleCardSubmit}>Save Entry</button>
+    </>
+  );
 };
 
 export default NewEntry;
--- src/components/SelectedTools.jsx
+++ src/components/SelectedTools.jsx
@@ -1,80 +1,70 @@
 import React from 'react';
 import Swal from 'sweetalert2';
 
-
 function SelectedTools(props) {
-    const options = [
-        { name: 'Select…', value: '' },
-        { name: 'JavaScript', value: 'JavaScript' },
-        { name: 'React', value: 'React' },
-        { name: 'Vanilla', value: 'Vanilla' },
-        { name: 'Other', value: 'Other' }
-    ];
+  const options = [
+    { name: 'Select…', value: '' },
+    { name: 'JavaScript', value: 'JavaScript' },
+    { name: 'React', value: 'React' },
+    { name: 'Vanilla', value: 'Vanilla' },
+    { name: 'Other', value: 'Other' },
+  ];
 
-    const handleToolDelete = (index) => {
-        const newOptions = [...props.selectedOptions];
-        newOptions.splice(index, 1);
-        props.setSelectedOptions(newOptions);
-    };
+  const handleToolDelete = (index) => {
+    const newOptions = [...props.selectedOptions];
+    newOptions.splice(index, 1);
+    props.setSelectedOptions(newOptions);
+  };
 
-    const handleSelect = (e) => {
-        if (props.selectedOptions.length > 5) {
-            Swal.fire({
-                title: 'Hm...',
-                text: "You can't add more than 5 tags.",
-                confirmButtonText: 'Ok',
-            });
-        } else if (props.selectedOptions.includes(e.target.value)) {
-            Swal.fire({
-                title: 'Hm...',
-                text: 'You have already added this tag!',
-                confirmButtonText: 'Ok',
-            });
-        } else {
-            props.setSelectedOptions([...props.selectedOptions, e.target.value])
-            localStorage.setItem('data', JSON.stringify(props.selectedOptions))
-        }
+  const handleSelect = (e) => {
+    if (props.selectedOptions.length > 5) {
+      Swal.fire({
+        title: 'Hm...',
+        text: "You can't add more than 5 tags.",
+        confirmButtonText: 'Ok',
+      });
+    } else if (props.selectedOptions.includes(e.target.value)) {
+      Swal.fire({
+        title: 'Hm...',
+        text: 'You have already added this tag!',
+        confirmButtonText: 'Ok',
+      });
+    } else {
+      props.setSelectedOptions([...props.selectedOptions, e.target.value]);
+      localStorage.setItem('data', JSON.stringify(props.selectedOptions));
     }
-    
-    return (
-        <>
-            <select
-                name='tool'
-                id='tool'
-                value={props.selectedOptions}
-                onChange={handleSelect}>
-                {options.map(option => (
-                    <option
-                        key={option.value}
-                        selected={option.value === null ? 'selected' : null}
-                        value={option.value}
-                        disabled={option.value === null ? true : null} 
-                    >
-                        {option.name}
-                    </option>
-            ))}
-            </select>
-            <ul>
-                {props.selectedOptions.map((option, ind) => {return (
-                    <>
-                    <li key={props.selectedOptions.length}>{option}</li>
-                    {(props.selectedOptions.length > 0)
-                    ?
-                    <>
-                        <button onClick={() => handleToolDelete(ind)}>X</button>
-                    </>
-                    :
-                    null}
+  };
 
-                    </>
-                    )}
-                )}
-            </ul>
-      </>
-    );
-  }
+  return (
+    <>
+      <select name="tool" id="tool" value={props.selectedOptions} onChange={handleSelect}>
+        {options.map((option) => (
+          <option
+            key={option.value}
+            selected={option.value === null ? 'selected' : null}
+            value={option.value}
+            disabled={option.value === null ? true : null}
+          >
+            {option.name}
+          </option>
+        ))}
+      </select>
+      <ul>
+        {props.selectedOptions.map((option, ind) => {
+          return (
+            <>
+              <li key={props.selectedOptions.length}>{option}</li>
+              {props.selectedOptions.length > 0 ? (
+                <>
+                  <button onClick={() => handleToolDelete(ind)}>X</button>
+                </>
+              ) : null}
+            </>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
 
-  export default SelectedTools;
-
-
-
+export default SelectedTools;
